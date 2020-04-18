f8a1bc1af80e8a0d39fdb8edf8608d9b993cb344
SerjKocherga <31800421+SerjKocherga@users.noreply.github.com>
2020-04-18 20:10:37 +0300

    Added more datailed description for library

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
-///This library contains common command and response types for client and daemon for their communication.
-///Depending on which command the client enters into the console, its type will be determined, the command will be serialized and then sent to the daemon.
-///The response from the daemon comes in a serialized form and deserialize, it is reduced to the type of response to which the request was sent from the client.
-///Next, information is output to the client in the console in accordance with the type of response.
+//!This library contains common command and response types for client and daemon for their communication.
 
 use serde::{Serialize, Deserialize};
 use std::net::IpAddr;
 use std::collections::HashMap;
 
+///Connection port for the daemon and the client
 pub const PORT: u16 = 3000; // now it connects to 3000
+///Pair of containers for response from ls command
 pub type LsResponseType = HashMap<IpAddr, Vec<String>>;
+///Pair of pair of containers for response from status command
 pub type StatusResponseType = (HashMap<String, Vec<IpAddr>>, HashMap<String, Vec<IpAddr>>);
 
+///Depending on which command the client enters into the console, its type will be determined, the command will be serialized and then sent to the daemon.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum CommandType {
     Share(String),
@@ -25,6 +26,7 @@ pub enum CommandType {
    In success a client gets enum with 1 or 2 or 3 field. Depends from what client was requested.
    If error - enum with 4th field.*/
 
+///The response from the daemon comes in a serialized form and deserialize, it is reduced to the type of response to which the request was sent from the client.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum ResponseType {
     ShareScan,
